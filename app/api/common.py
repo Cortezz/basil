852798b55f822864aa21b08a56ef0a141b35bffc
Cortezz <jospcortez@gmail.com>
2017-11-21 22:51:58 +0000

    Use API to create trip

--- app/api/common.py
+++ app/api/common.py
@@ -1,6 +1,6 @@
 def coordinate_pair(value):
     valid_pair = 'x' in value and 'y' in value \
-                 and isinstance(value['x'], float) and isinstance(value['y'], float)
+                 and isinstance(value['x'], int) and isinstance(value['y'], int)
     if not valid_pair:
         raise ValueError("Invalid list of coordinates")
     return value
--- app/api/routes.py
+++ app/api/routes.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 
 from app.api.resources import Trip, Trips
 
-api_v1 = Blueprint('api_v1', __name__, url_prefix='/v1')
+api_v1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 api = Api(api_v1)
 
 api.add_resource(Trip, '/trips/<trip_id>')
