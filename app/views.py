b8e44567c6d1615396842764df1c166eb1bb4945
Cortezz <jospcortez@gmail.com>
2017-11-21 22:34:57 +0000

    Add Basil API call

--- app/views.py
+++ app/views.py
@@ -6,4 +6,6 @@ home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def home():
-    return render_template('home.html', gmaps_api_key=current_app.config['GMAPS_API_KEY'])
+    return render_template('home.html',
+                           basil_api_endpoint=current_app.config['BASIL_API_ENDPOINT'],
+                           gmaps_api_key=current_app.config['GMAPS_API_KEY'])
