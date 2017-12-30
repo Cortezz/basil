6f61a774332a7eff644951baae61c2e31f2d2273
Cortezz <jospcortez@gmail.com>
2017-12-30 21:38:39 +0000

    Update base template to include title block

--- app/views.py
+++ app/views.py
@@ -6,6 +6,6 @@ home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def home():
-    return render_template('home.html',
+    return render_template('user/home.html',
                            basil_api_endpoint=current_app.config['BASIL_API_ENDPOINT'],
                            gmaps_api_key=current_app.config['GMAPS_API_KEY'])
